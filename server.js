18e83323b4ed424bb7eb74c6454132b7ef2227fe
Shin-Goz-Maeda <km.tks.mml710@gmail.com>
2023-02-22 00:41:11 +0900

    DBからinstagramItem情報とi商品情報を取得する処理を実装

--- server.js
+++ server.js
@@ -17,6 +17,33 @@ app.get("/", (req, res) => {
   res.send("<h1>hello</h1>");
 });
 
+// item情報をDBから取得する
+app.get("/getImage", (req, res) => {
+  let SQL = "SELECT * FROM ItemsInfo";
+
+  db.query(SQL, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+// instagram情報をDBから取得する
+app.get("/getInstagramImage", (req, res) => {
+  let SQL = "SELECT itemId, instagramEmbedCode FROM InstagramItemsInfo WHERE itemId = itemId";
+
+  db.query(SQL, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+
 app.listen(3001, () => {
   console.log(" 3001 Server Start!")
 });
