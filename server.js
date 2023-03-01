8a926ea86dc9b1db795d3105ae9f3c50450fc43e
Shin-Goz-Maeda <km.tks.mml710@gmail.com>
2023-03-02 03:01:45 +0900

    #6 指定のアイテム情報をDBから取得する処理を追加

--- server.js
+++ server.js
@@ -15,12 +15,13 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/item/1')
   res.send("<h1>hello</h1>");
 });
 
-// item情報をDBから取得する
+// すべてのitem情報をDBから取得する
 app.get("/getImage", (req, res) => {
-  let SQL = "SELECT * FROM ItemsInfo";
+  let SQL = "SELECT brand, itemCategory, itemName, itemImgUrl, id FROM ItemsInfo";
 
   db.query(SQL, (err, result) => {
     if (err) {
@@ -31,19 +32,18 @@ app.get("/getImage", (req, res) => {
   });
 });
 
-// instagram情報をDBから取得する
-app.get("/getInstagramImage", (req, res) => {
-  let SQL = "SELECT itemId, instagramEmbedCode FROM InstagramItemsInfo WHERE itemId = itemId";
-
-  db.query(SQL, (err, result) => {
+// 指定のitem情報をDBから取得する
+app.get("/item/:id", (req, res) => {
+  const id = req.params.id;
+  let SQL = "SELECT brand, itemCategory, itemName, itemImgUrl, itemInfo FROM ItemsInfo WHERE itemId = ?";
+  db.query(SQL, [id], (err, result) => {
     if (err) {
       console.log(err);
     } else {
-      res.send(result);
+      res.send(result[0]);
     }
   });
 });
-
 
 app.listen(3001, () => {
   console.log(" 3001 Server Start!")
