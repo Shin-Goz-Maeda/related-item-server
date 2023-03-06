bb9c80c055c841f2f630f3f1dc3fe548b6e279cd
Shin-Goz-Maeda <km.tks.mml710@gmail.com>
2023-03-06 23:18:57 +0900

    インデント修正

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const db = mysql.createConnection({
   host: process.env.DB_KEY1,
   user: process.env.DB_KEY2,
   password: process.env.DB_KEY3,
-  database: process.env.DB_KEY4,
-  });
+  database: process.env.DB_KEY4
+});
 
 app.use(cors());
 app.use(express.json());
